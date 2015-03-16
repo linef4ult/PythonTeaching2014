8a6d41628fcf6aef79a2edd1f22720d4eb64847d
Mark Foley <mark.foley@dit.ie>
2015-03-16 18:24:01 +0000

    added code to enable CORS in Tomcat

--- WMAP/make_server.sh
+++ WMAP/make_server.sh
@@ -91,7 +91,25 @@ echo "** replace # listen_addresses = 'localhost' with listen_addresses = '*'";
 echo "**";
 echo "** Tomcat";
 echo "** Add config info to enable CORS";
-./replace_str.py -r /etc/tomcat7/web.xml -s "</web-app>" -n "  <filter>\n    <filter-name>CorsFilter</filter-name>\n    <filter-class>org.apache.catalina.filters.CorsFilter</filter-class>\n    <init-param>\n      <param-name>cors.allowed.origins</param-name>\n      <param-value>*</param-value>\n    </init-param>\n</filter>\n<filter-mapping>\n    <filter-name>CorsFilter</filter-name>\n    <url-pattern>/*</url-pattern>\n</filter-mapping>\n\n</web-app>";
+echo "**";
+
+read -r -d '' CORS_STRING << EOM
+  <filter>
+    <filter-name>CorsFilter</filter-name>
+    <filter-class>org.apache.catalina.filters.CorsFilter</filter-class>
+    <init-param>
+      <param-name>cors.allowed.origins</param-name>
+      <param-value>*</param-value>
+    </init-param>
+  </filter>
+  <filter-mapping>
+    <filter-name>CorsFilter</filter-name>
+    <url-pattern>/*</url-pattern>
+  </filter-mapping>
+</web-app>
+EOM
+
+./replace_str.py -r /etc/tomcat7/web.xml -s "</web-app>" -n $CORS_STRING;
 
 
 echo "**";
